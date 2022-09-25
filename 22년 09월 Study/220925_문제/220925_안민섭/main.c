b2c4fed7a8062dfaff9d83b8995de4e1b8a9d6af
bonghwang25 <bonghwang25@gmail.com>
2022-09-25 13:42:46 +0900

    220925 월말평가 update

--- 22년 09월 Study/220925_문제/220925_안민섭/main.c
+++ 22년 09월 Study/220925_문제/220925_안민섭/main.c
@@ -1,15 +1,7 @@
 #include "run.h"
-
 	
 int main(){
-	char cmd[100] ={};
-	while(1){
-		printf("SSAFY > ");
-		gets(cmd);
-		system(cmd);
-		if(run(cmd))break;
-	}
-
+	run();
 
 	return 0;
 }
--- 22년 09월 Study/220925_문제/220925_안민섭/run.c
+++ 22년 09월 Study/220925_문제/220925_안민섭/run.c
@@ -1,7 +1,12 @@
 #include "run.h"
 
 int run(char cmd[100]){
-	if(!strcmp(cmd,"exit")) return 1;
+	while(1){
+		printf("SSAFY > ");
+		gets(cmd);
+		if(!strcmp(cmd,"exit")) return 1;
+		system(cmd);
+	}
 	//if(!strcmp(cmd,"ls")) system("ls -al");
 	return 0;
 }
