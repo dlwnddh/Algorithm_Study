92e736f7da8005ec85d21efbc4709f6adcc7174f
RK-DIAMOND <109578247+RK-DIAMOND@users.noreply.github.com>
2022-10-05 23:31:12 +0900

    내일dijkstra다까먹을예정ㅎㅎ
    술먹을거거든

--- 22년 09월 Study/220925_문제/220925_고경훈/1860_진기의 최고급 붕어빵.cpp
+++ 22년 09월 Study/220925_문제/220925_고경훈/1860_진기의 최고급 붕어빵.cpp
@@ -1,28 +1,43 @@
+// #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int 
+int available(int t, int M, int K) { // available num of product at time x
+    int time = t / M;
+    return K * time;
+}
 
-int main(){
+int main() {
     // data input phase
     int TC = 0;
     scanf("%d", &TC);
-    for(int tc = 1; tc <= TC; tc++){
+    for (int tc = 1; tc <= TC; tc++) {
+        bool flagok = true;
         int N, M, K;
         scanf("%d %d %d", &N, &M, &K);
         // N: num of customers
         // M: manufacturing period
         // K: num of product made on one period
         int secs[100];
-        for(int i = 0; i < N; i++) scanf("%d", &secs[i]);
-    // ******** //
+        for (int i = 0; i < N; i++) scanf("%d", &secs[i]);
+        // ******** //
 
+        // calculating phase
+        sort(secs, secs + N);
+        for (int i = 0; i < N; i++) {
+            // we need (i + 1) products at time secs[i]
+            if (i + 1 > available(secs[i], M, K)) { // if we don't have enough products
+                flagok = false;
+                break;
+            }
+        }
+        // ******** //
 
-
+        // printing phase
         printf("#%d ", tc);
-
-    }
-
-
-
-return 0;}
+        if (flagok) printf("Possible\n");
+        else printf("Impossible\n");
+    } // End of for loop (or single testcase)
+    return 0;
+} // End of main function
