c7d83fe77e48c2bcc6eb560bb0071c3be108fb7c
RK-DAIMOND <kgh001233@gmail.com>
2022-10-05 16:58:45 +0900

    최적경로D5아닌거같은데

--- 22년 09월 Study/220925_문제/220925_고경훈/1247_최적 경로.cpp
+++ 22년 09월 Study/220925_문제/220925_고경훈/1247_최적 경로.cpp
@@ -1,5 +1,4 @@
 // #define _CRT_SECURE_NO_WARNINGS
-#include <vector>
 #include <iostream>
 using namespace std;
 
--- 22년 09월 Study/220925_문제/220925_고경훈/13240_정사각형 글꼴.C
+++ 22년 09월 Study/220925_문제/220925_고경훈/13240_정사각형 글꼴.C
@@ -1,31 +1,30 @@
 // #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
-
+ 
 int T;
 int H;
 int W;
 int N;
 int lengths[1000];
 int maxl = -1; // Maximum value in lengths[] array
-
-
+ 
+ 
 int min(int a, int b){
     return (a < b) ? a : b;
 }
-
-
+ 
+ 
 void initlengths(){
     maxl = -1;
     for(int i = 0; lengths[i] != 0; i++) lengths[i] = 0;
 }
-
-
+ 
+ 
 void gettc(){
     scanf("%d", &T);
 }
-
-
+ 
+ 
 void getinput(){
     scanf("%d %d %d", &H, &W, &N);
     for(int x = 0; x < N; x++){
@@ -35,8 +34,8 @@ void getinput(){
         if(maxl < lengths[x]) maxl = lengths[x];
     }
 }
-
-
+ 
+ 
 int valid(int n){
     int maxcollen = W / n;
     int maxcolnum = H / n;
@@ -51,26 +50,26 @@ int valid(int n){
         else{
             currentcollen += (lwordnow + 1);
         }
-
+ 
         // After the addition of word, check "currentcollen"
         if(currentcollen > maxcollen){
             currentcollen = lwordnow;
             currentcolnum++;
         }
-
-
+ 
+ 
         if(currentcolnum > maxcolnum) return 0;
     }
     return 1;
 }
-
-
+ 
+ 
 int run(){
     for(int goal = min(H, W/maxl); goal > 0; goal--) if(valid(goal)) return goal;
     return 0;
 }
-
-
+ 
+ 
 int main(void){
     gettc();
     for(int tc = 1; tc <= T; tc++){
