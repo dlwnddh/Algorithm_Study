3da4a712101dba9df620f61ac4fc3707cc969b25
RK-DAIMOND <kgh001233@gmail.com>
2022-10-05 14:23:10 +0900

    OMG

--- 22년 09월 Study/220925_문제/220925_고경훈/1247_최적 경로.cpp
+++ 22년 09월 Study/220925_문제/220925_고경훈/1247_최적 경로.cpp
@@ -1,7 +1,9 @@
-#define _CRT_SECURE_NO_WARNINGS
+// #define _CRT_SECURE_NO_WARNINGS
+#include <vector>
 #include <iostream>
+using namespace std;
 
-struct coord{
+struct coord {
     int x;
     int y;
 };
@@ -11,40 +13,74 @@ int N;
 coord company;
 coord home;
 coord customer[11];
-coord emptycrd = {0, 0};
+coord emptycrd = { 0, 0 };
+bool used[11] = { false, };
 
-void initializer(){
+
+int min(int a, int b){
+    return (a > b) ? b : a;
+}
+
+
+void initializer() {
     N = 0;
     company = emptycrd;
     home = emptycrd;
-    for(int i = 0; i < 11; i++){
+    for (int i = 0; i < 11; i++) {
         customer[i] = emptycrd;
+        used[i] = false;
     }
 }
 
-void getinput(){
+
+int dist(coord a, coord b) {
+    int result = 0;
+    result += (a.x > b.x) ? a.x - b.x : b.x - a.x;
+    result += (a.y > b.y) ? a.y - b.y : b.y - a.y;
+    return result;
+}
+
+
+void getinput() {
     scanf("%d", &N);
     scanf("%d %d %d %d", &(company.x), &(company.y), &(home.x), &(home.y));
-    for(int i = 0; i < N; i++){
+    for (int i = 0; i < N; i++) {
         scanf("%d %d", &(customer[i].x), &(customer[i].y));
     }
 }
 
-int run(){
 
-
-    return 0;
+void visit(coord c, int num, int sumlen, int* res){ // From the "c", 
+    if(sumlen > *res) return;
+    if(num == N){
+        sumlen += dist(c, home);
+        *res = min(*res, sumlen);
+        return;
+    }
+    for(int i = 0; i < N; i++){
+        if(!used[i]){
+            used[i] = true;
+            visit(customer[i], num + 1, sumlen + dist(c, customer[i]), res);
+            used[i] = false;
+        }
+    }
 }
 
 
-int main(){
+int run() {
+#define BIG 2147483647
+    int result = BIG;
+    visit(company, 0, 0, &result);
+    return result;
+}
+
+
+int main() {
     scanf("%d", &TC);
-    for(int tc = 1; tc <= TC; tc++){
+    for (int tc = 1; tc <= TC; tc++) {
         initializer();
         getinput();
-
         printf("#%d %d\n", tc, run());
     } // End of single testcase
-
-
-return 0;}      
+    return 0;
+}
