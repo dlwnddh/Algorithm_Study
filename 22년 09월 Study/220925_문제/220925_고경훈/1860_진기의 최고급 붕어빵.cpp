7d3d0f1d70744d0bed3dc1b28e3667171ec266c1
RK-DIAMOND <109578247+RK-DIAMOND@users.noreply.github.com>
2022-10-05 23:15:11 +0900

    vec
    vec

---
+++ 22년 09월 Study/220925_문제/220925_고경훈/1860_진기의 최고급 붕어빵.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // data input phase
+    int TC = 0;
+    scanf("%d", &TC);
+    for(int tc = 1; tc <= TC; tc++){
+        int N, M, K;
+        scanf("%d %d %d", &N, &M, &K);
+        // N: num of customers
+        // M: manufacturing period
+        // K: num of product made on one period
+        int secs[100];
+        for(int i = 0; i < N; i++) scanf("%d", &secs[i]);
+    // ******** //
+
+    
+
+        printf("#%d ", tc);
+
+    }
+
+
+
+return 0;}
